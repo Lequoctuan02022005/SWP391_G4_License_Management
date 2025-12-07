cc9f8490a6865f723bd02db1012b6a10cf49ad2f
hunghuy1017 <huytqhe186506@fpt.edu.vn>
2025-12-07 22:02:43 +0700

    fix view tool list

---
+++ src/main/java/swp391/fa25/lms/config/WebConfig.java
@@ -0,0 +1,17 @@
+package swp391.fa25.lms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+        // Nếu chạy Windows cần đường dẫn tuyệt đối:
+        // .addResourceLocations("file:C:/YourProject/uploads/");
+    }
+}
