3d34460b359ebee5e17c5de725f014f39306fb42
Le Tuan <tuna10a6@gmail.com>
2025-12-19 16:10:13 +0700

    update filter của renew

--- src/main/java/swp391/fa25/lms/service/LicenseRenewLogService.java
+++ src/main/java/swp391/fa25/lms/service/LicenseRenewLogService.java
@@ -7,16 +7,60 @@ import org.springframework.stereotype.Service;
 import swp391.fa25.lms.model.LicenseRenewLog;
 import swp391.fa25.lms.repository.LicenseRenewLogRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class LicenseRenewLogService {
 
     private final LicenseRenewLogRepository repo;
-
-    public Page<LicenseRenewLog> findForCustomer(Long accountId, Pageable pageable) {
-        return repo.findByCustomer(accountId, pageable);
+    // ===== CUSTOMER =====
+    public Page<LicenseRenewLog> filterForCustomer(
+            Long customerId,
+            LocalDate fromDate,
+            LocalDate toDate,
+            Long minAmount,
+            Long maxAmount,
+            Pageable pageable
+    ) {
+        return repo.filterForCustomer(
+                customerId,
+                toStart(fromDate),
+                toEnd(toDate),
+                minAmount,
+                maxAmount,
+                pageable
+        );
+    }
+    // ===== SELLER =====
+    public Page<LicenseRenewLog> filterForSeller(
+            Long sellerId,
+            LocalDate fromDate,
+            LocalDate toDate,
+            Long minAmount,
+            Long maxAmount,
+            Pageable pageable
+    ) {
+        return repo.filterForSeller(
+                sellerId,
+                toStart(fromDate),
+                toEnd(toDate),
+                minAmount,
+                maxAmount,
+                pageable
+        );
     }
 
+    private LocalDateTime toStart(LocalDate date) {
+        if (date == null) return null;
+        return date.atStartOfDay();
+    }
+
+    private LocalDateTime toEnd(LocalDate date) {
+        if (date == null) return null;
+        return date.atTime(23, 59, 59);
+    }
     public Page<LicenseRenewLog> findAll(Pageable pageable) {
         return repo.findAll(pageable);
     }
