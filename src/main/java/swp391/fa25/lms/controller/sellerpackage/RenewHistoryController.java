deda14932ae989db4f3a5ae539d896e296bb9eb0
Le Tuan <tuna10a6@gmail.com>
2025-12-19 13:09:03 +0700

    update filter của renew

--- src/main/java/swp391/fa25/lms/controller/sellerpackage/RenewHistoryController.java
+++ src/main/java/swp391/fa25/lms/controller/sellerpackage/RenewHistoryController.java
@@ -4,15 +4,19 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import swp391.fa25.lms.model.Account;
 import swp391.fa25.lms.model.SellerSubscription;
 import swp391.fa25.lms.service.SellerSubscriptionService;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/seller-package/renew-history")
@@ -21,28 +25,59 @@ public class RenewHistoryController {
 
     private final SellerSubscriptionService service;
 
-    @PreAuthorize("hasAnyRole('SELLER','ADMIN')")
     @GetMapping
+    @PreAuthorize("hasAnyRole('SELLER','ADMIN')")
     public String view(
             HttpSession session,
             Pageable pageable,
-            Model model
+            Model model,
+            @RequestParam(required = false) String packageName,
+            @RequestParam(required = false) String status,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate
     ) {
         Account acc = (Account) session.getAttribute("loggedInAccount");
         if (acc == null) {
             return "redirect:/login";
         }
 
-        boolean isAdmin = acc.getRole().getRoleName().name().equals("ADMIN");
+        boolean isAdmin = false;
+        if (acc.getRole().getRoleName().name().equals("ADMIN")) {
+            isAdmin = true;
+        }
 
-        Page<SellerSubscription> page =
-                isAdmin
-                        ? service.findAll(pageable)
-                        : service.findForSeller(acc.getAccountId(), pageable);
+        Page<SellerSubscription> page;
 
+        if (isAdmin) {
+            page = service.filter(
+                    null,
+                    null,
+                    packageName,
+                    status,
+                    fromDate,
+                    toDate,
+                    pageable
+            );
+        } else {
+            page = service.filter(
+                    acc.getFullName(),
+                    null,
+                    packageName,
+                    status,
+                    fromDate,
+                    toDate,
+                    pageable
+            );
+        }
+        // giữ filter
         model.addAttribute("page", page);
         model.addAttribute("isAdmin", isAdmin);
-        model.addAttribute("type", "SELLER_PACKAGE");
+        model.addAttribute("packageName", packageName);
+        model.addAttribute("status", status);
+        model.addAttribute("fromDate", fromDate);
+        model.addAttribute("toDate", toDate);
 
         return "sellerpackage/renew-history";
     }
